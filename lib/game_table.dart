e71d34e00ff7ce88cab7e9433c981395b237036c
sindrehenriksen <sindrehenriksen93@gmail.com>
2023-06-29 00:47:43 +0200

    Add data to game table from database

--- lib/game_table.dart
+++ lib/game_table.dart
@@ -1,54 +1,66 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-class GameTableWidget extends StatelessWidget {
+class GameTableWidget extends StatefulWidget {
   const GameTableWidget({super.key});
 
   @override
+  GameTableWidgetState createState() => GameTableWidgetState();
+}
+
+class GameTableWidgetState extends State<GameTableWidget> {
+  final teamsRef = FirebaseDatabase.instance.ref().child('teams');
+  late Map<String, dynamic> _teamsData;
+
+  @override
+  void initState() {
+    super.initState();
+    _teamsData = Map<String, dynamic>();
+    teamsRef.once().then((DatabaseEvent databaseEvent) {
+      setState(() {
+        _teamsData = databaseEvent.snapshot.value as Map<String, dynamic>;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: const [
-        DataColumn(label: Text('Team')),
-        DataColumn(label: Text('R'), tooltip: 'Elo Rating'),
-        DataColumn(label: Text('GP'), tooltip: 'Games Played'),
-        DataColumn(label: Text('W'), tooltip: 'Wins'),
-        DataColumn(label: Text('L'), tooltip: 'Losses'),
-        DataColumn(label: Text('PD'), tooltip: 'Points Difference'),
-        DataColumn(label: Text('PF'), tooltip: 'Points For'),
-        DataColumn(label: Text('PA'), tooltip: 'Points Against'),
-      ],
-      rows: [
-        DataRow(cells: [
-          DataCell(Text('Team A')),
-          DataCell(Text('TBD')),
-          DataCell(Text('2')),
-          DataCell(Text('2')),
-          DataCell(Text('0')),
-          DataCell(Text('7')),
-          DataCell(Text('20')),
-          DataCell(Text('13')),
-        ]),
-        DataRow(cells: [
-          DataCell(Text('Team B')),
-          DataCell(Text('TBD')),
-          DataCell(Text('2')),
-          DataCell(Text('1')),
-          DataCell(Text('1')),
-          DataCell(Text('0')),
-          DataCell(Text('18')),
-          DataCell(Text('18')),
-        ]),
-        DataRow(cells: [
-          DataCell(Text('Team C')),
-          DataCell(Text('TBD')),
-          DataCell(Text('2')),
-          DataCell(Text('0')),
-          DataCell(Text('2')),
-          DataCell(Text('-7')),
-          DataCell(Text('13')),
-          DataCell(Text('20')),
-        ]),
-      ],
+    return StreamBuilder<DatabaseEvent>(
+      stream: teamsRef.onValue.map((event) => event),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          _teamsData = (snapshot.data!).snapshot.value as Map<String, dynamic>;
+        }
+
+        final rows = _teamsData.entries.map((entry) {
+          final teamData = entry.value;
+          final cells = <DataCell>[
+            DataCell(Text(teamData['teamName'])),
+            DataCell(Text('TBD')),
+            DataCell(Text('TBD')),
+            DataCell(Text('TBD')),
+            DataCell(Text('TBD')),
+            DataCell(Text('TBD')),
+            DataCell(Text('TBD')),
+          ];
+
+          return DataRow(cells: cells);
+        }).toList();
+
+        return DataTable(
+          columns: const [
+            DataColumn(label: Text('Team')),
+            //DataColumn(label: Text('R'), tooltip: 'Elo Rating'),
+            DataColumn(label: Text('GP'), tooltip: 'Games Played'),
+            DataColumn(label: Text('W'), tooltip: 'Wins'),
+            DataColumn(label: Text('L'), tooltip: 'Losses'),
+            DataColumn(label: Text('PD'), tooltip: 'Points Difference'),
+            DataColumn(label: Text('PF'), tooltip: 'Points For'),
+            DataColumn(label: Text('PA'), tooltip: 'Points Against'),
+          ],
+          rows: rows
+        );
+      },
     );
   }
 }
-
